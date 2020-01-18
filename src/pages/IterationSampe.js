12639f814d9e2d0edca4a9de43518d8a6e5308e1
P-bin <qkrgksqls9@gmail.com>
2020-01-19 01:21:38 +0900

    Iteration 응용 2

--- src/pages/IterationSampe.js
+++ src/pages/IterationSampe.js
@@ -9,17 +9,40 @@ const IterationSampe = () => {
     //     </ul>
     // )
     const [ names, setNames ] = useState([
-        { id: '0', text: '눈사람'},
-        { id: '1', text: '얼음'},
-        { id: '2', text: '눈'},
-        { id: '3', text: '바람'}
+        { id: 0, text: '눈사람'},
+        { id: 1, text: '얼음'},
+        { id: 2, text: '눈'},
+        { id: 3, text: '바람'}
     ]);
     const [ inputText, setInputText ] = useState('');
     const [ nextId, setNextId ] = useState(4);
 
-    const nameList = names.map(name => <li kye={name.id}>{name.text}</li>);
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id : nextId,
+            text: inputText
+        })
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    }
+    const onRemove = id => {
+        const nextNames = names.filter(name => name.id !== id);
+        setNames(nextNames);
+    }
+
+    const nameList = names.map(name => (
+        <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
     return (
-        <ul>{nameList}</ul>
+        <>
+            <input value={inputText} onChange={onChange}/>
+            <button onClick={onClick}>Add</button>
+            <ul>{nameList}</ul>
+        </>
     )
 }
 
