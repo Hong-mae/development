6db821a71d3a17fe0c8371775620cf40e2c7df6f
P-bin <qkrgksqls9@gmail.com>
2020-01-19 00:54:03 +0900

    Iteration 응용 1

--- src/pages/IterationSampe.js
+++ src/pages/IterationSampe.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const IterationSampe = () => {
-    const names = [ "눈사람", "얼음", "눈", "바람" ];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+    // const names = [ "눈사람", "얼음", "눈", "바람" ];
+    // const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+    // return (
+    //     <ul>
+    //         {nameList}
+    //     </ul>
+    // )
+    const [ names, setNames ] = useState([
+        { id: '0', text: '눈사람'},
+        { id: '1', text: '얼음'},
+        { id: '2', text: '눈'},
+        { id: '3', text: '바람'}
+    ]);
+    const [ inputText, setInputText ] = useState('');
+    const [ nextId, setNextId ] = useState(4);
+
+    const nameList = names.map(name => <li kye={name.id}>{name.text}</li>);
     return (
-        <ul>
-            {nameList}
-        </ul>
+        <ul>{nameList}</ul>
     )
 }
 
