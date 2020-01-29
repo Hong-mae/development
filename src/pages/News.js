dcd17b6aaa6b72ef6342e1b3d17d2077edc447bc
P-bin <qkrgksqls9@gmail.com>
2020-01-30 00:21:02 +0900

    실제 뉴스 api 끌어오기

--- src/pages/News.js
+++ src/pages/News.js
@@ -4,11 +4,24 @@ import axios from 'axios'
 const News = () => {
     const [data, setData] = useState(null)
     
-    const onClick = (e) => {
-        axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
+    // const onClick = (e) => {
+    //     axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
+    //         setData(res.data);
+    //     });
+    // }
+
+    const onClick = async () => {
+        try {
+            const res = await axios.get(
+                "https://newsapi.org/v2/top-headlines?country=kr&apiKey=455e3b6a24194aec894473ff6d2886b1",
+            );
+
             setData(res.data);
-        });
+        } catch (e) {
+            console.log(e);
+        }
     }
+    
     return (
         <div>
            <div><button onClick={onClick}>불러오기</button></div> 
