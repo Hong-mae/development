1d486686af2bb8aa9333ad25c68f6c4f5da6cac2
P-bin <qkrgksqls9@gmail.com>
2020-01-29 21:43:16 +0900

    complete todoapp

--- src/Components/To_Do_Apps/TodoListItem.js
+++ src/Components/To_Do_Apps/TodoListItem.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline, MdRemove } from 'react-icons/md'
+import cn from 'classnames'
+import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md'
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+    const { id, text, checked } = todo;
+
+    const onClick = (e) => {
+        onRemove(id);
+    }
     return (
         <div className='TodoListItem'>
-            <div className='checkbox'>
-                <MdCheckBoxOutlineBlank />
-                <div className='text'>할 일</div>
-                <div className='remove'>
-                    <MdRemoveCircleOutline />
-                </div>
+            <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+                {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+                <div className='text'>{text}</div>
+            </div>
+            <div className='remove' onClick={onClick}>
+                <MdRemoveCircleOutline />
             </div>
         </div>
     )
--- src/pages/To_Do_Apps.js
+++ src/pages/To_Do_Apps.js
@@ -1,18 +1,59 @@
-import React, { Component } from 'react'
-
+import React, { useState, useRef, useCallback } from 'react'
 import TodoTemplate from '../Components/To_Do_Apps/TodoTemplate'
 import TodoInsert from '../Components/To_Do_Apps/TodoInsert'
+import TodoList from '../Components/To_Do_Apps/TodoList'
 
-export class To_Do_Apps extends Component {
-    render() {
-        return (
-            <div className='todoapp'>
-                <TodoTemplate >
-                    <TodoInsert />
-                </TodoTemplate>
-            </div>
+const To_Do_Apps = () => {
+    const [ todos, setTodos ] = useState([
+        {
+            id: 0,
+            text: 'todo 0',
+            checked: true,
+        },
+        {
+            id: 1,
+            text: 'todo 1',
+            checked: true,
+        },
+        {
+            id: 2,
+            text: 'todo 2',
+            checked: false,
+        },
+    ]);
+
+    const nextId = useRef(3);
+
+    const onInsert = useCallback( text =>{
+        const todo = {
+            id: nextId.current,
+            text,
+            checked: false,
+        };
+        setTodos(todos.concat(todo));
+        nextId.current += 1;
+    }, [todos]);
+
+    const onRemove = useCallback( id => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }, [todos]);
+
+    const onToggle = useCallback(id => {
+        setTodos(
+            todos.map(todo => 
+                todo.id === id ? { ...todo, checked: !todo.checked } : todo
+            )
         )
-    }
+    })
+
+    return (
+        <div className='todoapp'>
+            <TodoTemplate >
+                <TodoInsert onInsert={onInsert}/>
+                <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+            </TodoTemplate>
+        </div>
+    )
 }
 
 export default To_Do_Apps
