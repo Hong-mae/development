7506e66800ec4cbec6e29cb294a7c70352d9c5d2
P-bin <qkrgksqls9@gmail.com>
2020-01-31 08:48:57 +0900

    Update Categories

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
     const [articles, setArticles] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
         const fetchData = async () => {
             setLoading(true);
             try {
+                const query = category === 'all' ? '' : `&category=${category}`;
                 const res = await axios.get(
-                    "https://newsapi.org/v2/top-headlines?country=kr&apiKey=455e3b6a24194aec894473ff6d2886b1"
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=455e3b6a24194aec894473ff6d2886b1`
                 );
                 setArticles(res.data.articles);
             } catch (e) {
@@ -34,7 +35,7 @@ const NewsList = () => {
             setLoading(false);
         }
         fetchData();
-    }, [])
+    }, [category])
 
     if (loading) {
         return <NewsListBlock>대기 중....</NewsListBlock>
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 
 import NewsList from '../Components/News/NewsList'
+import Categories from '../Components/News/Categories'
 
 const News = () => {
+    const [category, setCategory] = useState('all');
+    const onSelect = useCallback(category => setCategory(category), []);
     return (
-        <NewsList/>
+        <>
+            <Categories category={category} onSelect={onSelect}/>
+            <NewsList category={category}/>
+        </>
     )
 }
 
