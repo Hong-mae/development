7c6239993e8ac00ac3d47645817270a9edae269f
P-bin <qkrgksqls9@gmail.com>
2020-01-30 23:53:06 +0900

    최신 뉴스 20개 가져오기

---
+++ src/Components/News/NewsList.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import NewsItems from './NewsItems'
+import axios from 'axios'
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (max-width: 768px) {
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`;
+
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const res = await axios.get(
+                    "https://newsapi.org/v2/top-headlines?country=kr&apiKey=455e3b6a24194aec894473ff6d2886b1"
+                );
+                setArticles(res.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, [])
+
+    if (loading) {
+        return <NewsListBlock>대기 중....</NewsListBlock>
+    }
+
+    if(!articles){
+        return null
+    }
+
+    return (
+        <NewsListBlock>
+                {articles.map(article => (
+                    <NewsItems key={articles.url} article={article}/>
+                ))}
+        </NewsListBlock>
+    )
+}
+
+export default NewsList
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React from 'react'
+
+import NewsList from '../Components/News/NewsList'
 
 const News = () => {
-    const [data, setData] = useState(null)
-    
-    // const onClick = (e) => {
-    //     axios.get("https://jsonplaceholder.typicode.com/todos/1").then(res => {
-    //         setData(res.data);
-    //     });
-    // }
-
-    const onClick = async () => {
-        try {
-            const res = await axios.get(
-                "https://newsapi.org/v2/top-headlines?country=kr&apiKey=455e3b6a24194aec894473ff6d2886b1",
-            );
-
-            setData(res.data);
-        } catch (e) {
-            console.log(e);
-        }
-    }
-    
     return (
-        <div>
-           <div><button onClick={onClick}>불러오기</button></div> 
-           {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true}/>}
-        </div>
+        <NewsList/>
     )
 }
 
