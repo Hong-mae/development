f9e270d37281f2c773d822ae9d90eef43f4e6cc8
verify3220 <qkrgksqls9@gmail.com>
2020-02-12 23:58:39 +0900

    usePromise 커스텀 Hook 만들기

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import NewsItems from './NewsItems'
 import axios from 'axios'
+import usePromise from '../../lib/usePromise'
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -17,34 +18,56 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
+    // custom Hook 적용 전
+    // const [articles, setArticles] = useState(null);
+    // const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const res = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=455e3b6a24194aec894473ff6d2886b1`
-                );
-                setArticles(res.data.articles);
-            } catch (e) {
-                console.log(e);
-            }
-            setLoading(false);
-        }
-        fetchData();
-    }, [category])
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         setLoading(true);
+    //         try {
+    //             const query = category === 'all' ? '' : `&category=${category}`;
+    //             const res = await axios.get(
+    //                 `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=455e3b6a24194aec894473ff6d2886b1`
+    //             );
+    //             setArticles(res.data.articles);
+    //         } catch (e) {
+    //             console.log(e);
+    //         }
+    //         setLoading(false);
+    //     }
+    //     fetchData();
+    // }, [category])
 
-    if (loading) {
+    // if (loading) {
+    //     return <NewsListBlock>대기 중....</NewsListBlock>
+    // }
+
+    // if(!articles){
+    //     return null
+    // }
+
+    // custom Hook 적용 후
+    const [ loading, response, error ] = usePromise(()=> {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=455e3b6a24194aec894473ff6d2886b1`
+        )
+    }, [ category ]);
+
+    if(loading){
         return <NewsListBlock>대기 중....</NewsListBlock>
     }
 
-    if(!articles){
-        return null
+    if(!response){
+        return null;
     }
 
+    if(error) {
+        return <NewsListBlock>에러 발생!!!</NewsListBlock>
+    }
+
+    const { articles } = response.data;
     return (
         <NewsListBlock>
                 {articles.map(article => (
