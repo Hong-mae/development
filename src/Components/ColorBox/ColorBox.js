5279fd1088e3a9fd3f9e9900c2e49923a417261b
verify3220 <qkrgksqls9@gmail.com>
2020-02-13 00:06:08 +0900

    Context API 기초 2 Provider

--- src/Components/ColorBox/ColorBox.js
+++ src/Components/ColorBox/ColorBox.js
@@ -3,17 +3,21 @@ import ColorContext from '../../Contexts/color'
 
 const ColorBox = () => {
     return (
-        <ColorContext.Consumer>
-            {value => (
-                <div
-                    style = {{
-                        width: '64px',
-                        height: '64px',
-                        background: value.color
-                    }}
-                />
-            )}
-        </ColorContext.Consumer>
+        <ColorContext.Provider value={{color : 'red'}}>
+            <div>
+                <ColorContext.Consumer>
+                    {value => (
+                        <div
+                            style = {{
+                                width: '64px',
+                                height: '64px',
+                                background: value.color
+                            }}
+                        />
+                    )}
+                </ColorContext.Consumer>
+            </div>
+        </ColorContext.Provider>
     )
 }
 
