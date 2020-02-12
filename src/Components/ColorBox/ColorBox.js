d93090d3113d601a9c95f2683c669c20cfac9c36
verify3220 <qkrgksqls9@gmail.com>
2020-02-13 00:50:14 +0900

    클래스 형에서 사용하는 법 static 사용

--- src/Components/ColorBox/ColorBox.js
+++ src/Components/ColorBox/ColorBox.js
@@ -1,50 +1,14 @@
 import React from 'react'
-import ColorContext, { ColorProvider, ColorConsumer } from '../../Contexts/color'
+import { ColorProvider } from '../../Contexts/color'
 import SelectColors from './SelectColors'
+import ColorContent from './ColorContent'
 
 const ColorBox = () => {
     return (
         <ColorProvider>
             <div>
                 <SelectColors/>
-                <ColorConsumer>
-                    {/* {value => (
-                        <>
-                            <div
-                                style = {{
-                                    width: '64px',
-                                    height: '64px',
-                                    background: value.state.color
-                                }}
-                            />
-                            <div
-                                style = {{
-                                    width: '32px',
-                                    height: '32px',
-                                    background: value.state.subcolor
-                                }}
-                            />
-                        </>
-                    )} */}
-                    {({state}) => (
-                        <>
-                            <div
-                                style = {{
-                                    width: '64px',
-                                    height: '64px',
-                                    background: state.color
-                                }}
-                            />
-                            <div
-                                style = {{
-                                    width: '32px',
-                                    height: '32px',
-                                    background: state.subcolor
-                                }}
-                            />
-                        </>
-                    )}
-                </ColorConsumer>
+                <ColorContent/>
             </div>
         </ColorProvider>
     )
